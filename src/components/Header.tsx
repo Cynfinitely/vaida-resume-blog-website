3be3f30b14278c0dd53e82b60a099d34712635ff
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-06-01 21:45:11 +0300

    styling update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-primaryColor w-screen  text-secondaryColor text-4xl font-semibold'>We are only as strong as the weakest link</div>
+    <div className='bg-primaryColor w-screen  text-secondaryColor text-4xl font-semibold text-center p-6'>We are only as strong as the weakest link</div>
   )
 }
 
