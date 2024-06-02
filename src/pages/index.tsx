7b61c454838816da17b9accfaa6d1693df59149d
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-06-03 00:01:36 +0300

    styling continues.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,23 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div className='flex flex-col items-center justify-center space-y-4'>
-      <section className='bg-primaryColor w-full p-4'>
-        <h1 className='text-2xl font-bold'>Mission</h1>
+      <section className='bg-primaryColor w-full p-4 flex flex-col justify-center items-center text-secondaryColor text-4xl font-semibold'>
+        <h1 className=''>Mission</h1>
         <p className='mt-2'>First paragraph of the mission.</p>
         <p className='mt-2'>Second paragraph of the mission.</p>
         <p className='mt-2'>Third paragraph of the mission.</p>
       </section>
-      <section className='flex flex-col items-center justify-center space-y-2'>
+      <section className='flex  items-center justify-center space-y-2'>
+        <div>
         <img src='path_to_your_image.jpg' alt='Description of the image' className='w-full h-64 object-cover' />
         <figcaption className='text-center'>Caption for the image</figcaption>
+        </div>
+
+        <div>
         <h2 className='text-xl font-bold'>About</h2>
         <p>Paragraph about the topic.</p>
+        </div>
+
       </section>
     </div>
   );
