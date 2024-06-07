b9a14451193c9877dd322ee1bcc5b28534cd2341
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-06-07 22:33:56 +0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='bg-primaryColor w-screen  text-secondaryColor text-4xl font-semibold text-center p-6'>Footer</div>
-  )
-}
+    <div className="bg-primaryColor w-screen  text-secondaryColor text-4xl font-semibold text-center p-6">
+      Vaida
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
