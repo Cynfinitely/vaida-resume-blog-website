13d6f085573847e200bb3343b10cc5f6e5d4a7e2
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-06-20 22:57:41 +0300

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-primaryColor w-screen  text-secondaryColor text-4xl font-semibold text-center p-6">
-      Vaida Resume Blog Website, 2024 , All rights reserved.
+      Vaida Resume Blog Website, 2024 , All rights reserved. Contact in any
+      case.
     </div>
   );
 };
