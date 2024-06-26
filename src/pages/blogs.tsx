f5119d4b93cd02ecef05d0bb1b84203ba82bdc82
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-06-26 22:53:21 +0300

    .

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const blogs = () => {
-  return <div>Blogs & Articles</div>;
+  return (
+    <div>
+      <h1>Blogs & Articles</h1>
+    </div>
+  );
 };
 
 export default blogs;
